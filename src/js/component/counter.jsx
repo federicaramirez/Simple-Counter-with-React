6b8aff81c84af9ff12c20a9a70f1c421951b06d9
federicaramirez <federicaaramirez@gmail.com>
2022-10-04 20:39:14 +0000

    comentario

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+//include images into your bundle
+// import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const SecondCounter = () => {
+	return (
+		<div className="text-center">
+		
+		</div>
+	);
+};
+
+export default SecondCounter;
